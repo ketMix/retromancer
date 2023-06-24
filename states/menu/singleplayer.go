093e649da2505b6d5bafa206fa53cbd21e6850f9
kts of kettek <kts@kettek.net>
2023-06-23 19:10:23 -0700

    Add player "columns"

--- states/menu/singleplayer.go
+++ states/menu/singleplayer.go
@@ -4,25 +4,18 @@ import (
 	"ebijam23/resources"
 	"ebijam23/states"
 	"ebijam23/states/game"
-	"math/rand"
-	"strings"
+	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 type SinglePlayer struct {
-	clickSound     *resources.Sound
-	items          []resources.MenuItem
-	hats           []string
-	hatIndex       int
-	hatItem        *resources.SpriteItem
-	hatText        *resources.TextItem
-	controllerItem *resources.SpriteItem
-	useController  bool
-	//
-	localPlayers []*game.LocalPlayer
-	overlay      game.Overlay
+	clickSound    *resources.Sound
+	items         []resources.MenuItem
+	backItem      *resources.TextItem
+	playerEntries [2]*PlayerEntry
+	overlay       game.Overlay
 }
 
 func (s *SinglePlayer) Init(ctx states.Context) error {
@@ -30,183 +23,29 @@ func (s *SinglePlayer) Init(ctx states.Context) error {
 	//
 	s.clickSound = ctx.Manager.GetAs("sounds", "click", (*resources.Sound)(nil)).(*resources.Sound)
 
-	// Set up local player.
-	s.localPlayers = append(s.localPlayers, game.NewLocalPlayer())
+	s.playerEntries[0] = &PlayerEntry{
+		player: game.NewLocalPlayer(),
+	}
+	// Dummy player entry for 2nd player.
+	s.playerEntries[1] = &PlayerEntry{}
 
-	// Load in our hats.
-	s.hats = ctx.Manager.GetNamesWithPrefix("images", "hat-")
-	s.hatIndex = int(rand.Int31n(int32(len(s.hats))))
+	for _, e := range s.playerEntries {
+		e.Init(ctx)
+	}
 
-	centerX := 320.0
-	leftX := centerX - 50.0
-	rightX := centerX + 50.0
-	x := 320.0
-	y := 30.0
-
-	s.items = append(s.items, &resources.TextItem{
-		X:    centerX,
-		Y:    y,
-		Text: ctx.L("Hat"),
-		Callback: func() bool {
-			return false
-		},
-	})
-
-	y += 30.0
-
-	s.items = append(s.items, &resources.SpriteItem{
-		X:      leftX,
-		Y:      y,
-		Sprite: resources.NewSprite(ctx.Manager.Get("images", "arrow-left").(*ebiten.Image)),
-		Callback: func() bool {
-			s.clickSound.Play(1.0)
-			s.hatIndex--
-			if s.hatIndex < 0 {
-				s.hatIndex = len(s.hats) - 1
-			}
-			s.SyncHat(ctx)
-			return false
-		},
-	})
-	s.items[len(s.items)-1].(*resources.SpriteItem).Sprite.Centered = true
-
-	s.items = append(s.items, &resources.SpriteItem{
-		X:      centerX,
-		Y:      y,
-		Sprite: resources.NewSprite(ctx.Manager.Get("images", s.hats[s.hatIndex]).(*ebiten.Image)),
-		Callback: func() bool {
-			return false
-		},
-	})
-	s.hatItem = s.items[len(s.items)-1].(*resources.SpriteItem)
-	s.hatItem.Sprite.Centered = true
-	s.items[len(s.items)-1].(*resources.SpriteItem).Sprite.Scale = 2.0
-
-	s.items = append(s.items, &resources.TextItem{
-		X:    centerX,
-		Y:    y + 20,
-		Text: "",
-		Callback: func() bool {
-			return false
-		},
-	})
-	s.hatText = s.items[len(s.items)-1].(*resources.TextItem)
-
-	s.SyncHat(ctx)
-
-	s.items = append(s.items, &resources.SpriteItem{
-		X:      rightX,
-		Y:      y,
-		Sprite: resources.NewSprite(ctx.Manager.Get("images", "arrow-right").(*ebiten.Image)),
-		Callback: func() bool {
-			s.clickSound.Play(1.0)
-			s.hatIndex++
-			if s.hatIndex >= len(s.hats) {
-				s.hatIndex = 0
-			}
-			s.SyncHat(ctx)
-			return false
-		},
-	})
-	s.items[len(s.items)-1].(*resources.SpriteItem).Sprite.Centered = true
-
-	y += s.items[len(s.items)-1].(*resources.SpriteItem).Sprite.Height() + 60
-
-	// Controller
-	s.items = append(s.items, &resources.TextItem{
-		X:    centerX,
-		Y:    y,
-		Text: ctx.L("Input"),
-		Callback: func() bool {
-			return false
-		},
-	})
-
-	y += 30.0
-
-	s.items = append(s.items, &resources.SpriteItem{
-		X:      leftX,
-		Y:      y,
-		Sprite: resources.NewSprite(ctx.Manager.Get("images", "arrow-left").(*ebiten.Image)),
-		Callback: func() bool {
-			s.clickSound.Play(1.0)
-			s.useController = !s.useController
-			s.SyncController(ctx)
-			return false
-		},
-	})
-	s.items[len(s.items)-1].(*resources.SpriteItem).Sprite.Centered = true
-
-	s.items = append(s.items, &resources.SpriteItem{
-		X:      centerX,
-		Y:      y,
-		Sprite: resources.NewSprite(ctx.Manager.Get("images", "keyboard").(*ebiten.Image)),
-		Callback: func() bool {
-			return false
-		},
-	})
-	s.controllerItem = s.items[len(s.items)-1].(*resources.SpriteItem)
-	s.items[len(s.items)-1].(*resources.SpriteItem).Sprite.Centered = true
-
-	s.items = append(s.items, &resources.SpriteItem{
-		X:      rightX,
-		Y:      y,
-		Sprite: resources.NewSprite(ctx.Manager.Get("images", "arrow-right").(*ebiten.Image)),
-		Callback: func() bool {
-			s.clickSound.Play(1.0)
-			s.useController = !s.useController
-			s.SyncController(ctx)
-			return false
-		},
-	})
-	s.items[len(s.items)-1].(*resources.SpriteItem).Sprite.Centered = true
-
-	// Start and stuff
-	x = 320.0
-	y = 320.0
-	s.items = append(s.items, &resources.TextItem{
-		X:    x,
-		Y:    y,
-		Text: ctx.L("Start"),
+	s.backItem = &resources.TextItem{
+		Text: ctx.L("Back"),
+		X:    30,
+		Y:    335,
 		Callback: func() bool {
 			s.clickSound.Play(1.0)
 			ctx.StateMachine.PopState()
-			ctx.StateMachine.PushState(&game.World{
-				StartingMap: "start",
-				Players: []game.Player{
-					s.localPlayers[0],
-				},
-			})
-			return true
+			return false
 		},
-	})
-	y -= 50 + 16
+	}
+	s.items = append(s.items, s.backItem)
 
 	return nil
-}
-
-func (s *SinglePlayer) SyncHat(ctx states.Context) {
-	s.hatItem.Sprite.SetImage(ctx.Manager.Get("images", s.hats[s.hatIndex]).(*ebiten.Image))
-
-	s.hatText.Text = strings.TrimPrefix(s.hats[s.hatIndex], "hat-")
-
-	s.localPlayers[0].SetHat(s.hats[s.hatIndex])
-}
-
-func (s *SinglePlayer) SyncController(ctx states.Context) {
-	if s.useController && len(ebiten.AppendGamepadIDs(nil)) == 0 {
-		s.useController = false
-	}
-
-	if s.useController {
-		gamepadIDs := ebiten.AppendGamepadIDs(nil)
-		s.localPlayers[0].GamepadID = int(gamepadIDs[0])
-
-		s.controllerItem.Sprite.SetImage(ctx.Manager.Get("images", "controller").(*ebiten.Image))
-	} else {
-		s.localPlayers[0].GamepadID = 0
-		s.controllerItem.Sprite.SetImage(ctx.Manager.Get("images", "keyboard").(*ebiten.Image))
-	}
 }
 
 func (s *SinglePlayer) Finalize(ctx states.Context) error {
@@ -219,6 +58,23 @@ func (s *SinglePlayer) Enter(ctx states.Context) error {
 
 func (s *SinglePlayer) Update(ctx states.Context) error {
 	s.overlay.Update(ctx)
+
+	// Check for controller button hit to activate player 2.
+	for _, gamepadID := range ebiten.AppendGamepadIDs(nil) {
+		if inpututil.IsGamepadButtonJustPressed(gamepadID, ebiten.GamepadButton9) {
+			pl := game.NewLocalPlayer()
+			s.playerEntries[1].player = pl
+			s.playerEntries[1].useController = true
+			s.playerEntries[1].SyncController(ctx)
+			pl.GamepadID = int(gamepadID)
+		}
+	}
+
+	x := -(len(s.playerEntries) - 1) * 150 / 2
+	for i, e := range s.playerEntries {
+		e.Update(ctx, float64(x+i*150))
+	}
+
 	x, y := ebiten.CursorPosition()
 	for _, m := range s.items {
 		m.CheckState(float64(x), float64(y))
@@ -236,6 +92,10 @@ func (s *SinglePlayer) Update(ctx states.Context) error {
 }
 
 func (s *SinglePlayer) Draw(ctx states.DrawContext) {
+	ctx.Text.SetColor(color.White)
+	for _, e := range s.playerEntries {
+		e.Draw(ctx)
+	}
 	for _, m := range s.items {
 		m.Draw(ctx)
 	}
