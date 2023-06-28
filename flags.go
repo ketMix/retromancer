f14abceb736186f24e44abc1096b4c6831b6445d
kts of kettek <kts@kettek.net>
2023-06-27 19:01:46 -0700

    Add flags for network configs

--- flags.go
+++ flags.go
@@ -1,12 +1,15 @@
 package main
 
 type Flags struct {
-	Locale      string
-	Font        string
-	MusicVolume float64
-	SoundVolume float64
-	Muted       bool
-	Fullscreen  bool
-	SkipIntro   bool
-	Map         string
+	Locale          string
+	Font            string
+	MusicVolume     float64
+	SoundVolume     float64
+	Muted           bool
+	Fullscreen      bool
+	SkipIntro       bool
+	Map             string
+	NetBufferSize   int
+	NetDataShards   int
+	NetParityShards int
 }
